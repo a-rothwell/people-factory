08d1b7bea3c96a4188ef1d6621d7e6311bc401db
Andrew Rothwell <a_rothwell@outlook.com>
2017-06-05 14:35:52 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const personForm = document.querySelector('form#personForm')
-
 function handelSubmit(ev){
     ev.preventDefault()
     const f = ev.target
-    console.log(f.personName.value)
+    const name = f.personName.value
+    const color = f.favoriteColor.value
+    let CSScolor = color.replace(/\s/g,'');
+    const newParagraph = document.createElement("P");
+    const t = document.createTextNode(color + " is " + name + "\'s  favorite color");
+    newParagraph.appendChild(t);
+    newParagraph.style.backgroundColor = CSScolor
+    newParagraph.style.color = 'white'
+    if(newParagraph.style.backgroundColor == ''){
+        newParagraph.style.color = 'black'
+    }
+    document.body.appendChild(newParagraph);
 }
-
 personForm.addEventListener('submit', handelSubmit)
