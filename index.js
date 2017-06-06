b5499bf220e3c6d6f58f41895691e4e594f60a4d
Andrew Rothwell <a_rothwell@outlook.com>
2017-06-06 10:31:42 -0400

    more functions to abstract

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const personForm = document.querySelector('form#personForm')
-function handelSubmit(ev){
+
+function handelSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const name = f.personName.value
     const color = f.favoriteColor.value
-    let CSScolor = color.replace(/\s/g,'');
+    const newParagraph = onCreateParagraph(name,color)
+    document.body.appendChild(newParagraph);
+    document.body.appendChild(document.createElement("HR"))
+    const details = document.querySelector('#details')
+    details.innerHTML = `<em>${name}</em>`
+}
+
+function onCreateParagraph(name,color){
+    let CSScolor = color.replace(/\s/g, '')
     const newParagraph = document.createElement("P")
     const t = document.createTextNode(color + " is " + name + "\'s  favorite color")
     newParagraph.appendChild(t)
     newParagraph.style.backgroundColor = CSScolor
     newParagraph.style.color = 'white'
-    if(newParagraph.style.backgroundColor == ''){
-        newParagraph.style.color = '#d3b'
-    }
-    document.body.appendChild(newParagraph);
-    document.body.appendChild(document.createElement("HR"))
-
-    const details = document.querySelector('#details')
-    details.innerHTML = name
+    return newParagraph
 }
 personForm.addEventListener('submit', handelSubmit)
